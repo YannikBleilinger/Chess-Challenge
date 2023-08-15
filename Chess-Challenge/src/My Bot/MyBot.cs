25c99bbfeac12426ac905b616ceebde939f6462e
Yannik Bleilinger <yannik.bleilinger@uni-ulm.de>
2023-08-15 19:41:55 +0200

    feat: Added iterative deepening, still contains bugs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,26 +4,38 @@ using System.Numerics;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using Microsoft.CodeAnalysis;
 
+//todo: bot stürzt ab wenn die zeit abläuft und die max tiefe noch nicht erreicht wurde
 public class MyBot : IChessBot
 {
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-    const int MAX_DEPHT = 5; // You can adjust this value depending on how much time you have.
+    const int MAX_DEPHT = 10; // You can adjust this value depending on how much time you have.
+    private bool isWhite;
+    private int timeLimit = 10000;
+    private Move lastBestMove = Move.NullMove;
 
     private Dictionary<ulong, TranspositionEntry> transpositionTable = new();
 
     private int evaluatedPositions;
+    
     public Move Think(Board board, Timer timer)
     {
-        
         //weiß ist maximising, schwarz ist minimizing spieler
-        var (bestMove, eval) = AlphaBeta(board, MAX_DEPHT, int.MinValue, int.MaxValue, board.IsWhiteToMove);
-        Console.WriteLine("Evaluated " + evaluatedPositions + " Positions. Best value: " + eval);
+        //var (bestMove, eval) = AlphaBeta(board, MAX_DEPHT, int.MinValue, int.MaxValue, isWhite = board.IsWhiteToMove);
+        timeLimit = (int)Math.Round(timer.IncrementMilliseconds + timer.MillisecondsRemaining * 0.1);
+        Console.WriteLine("Time Limit "+timeLimit);
+        var bestMove = IterativeDeepening(board,timer);
+        Console.WriteLine("Evaluated " + evaluatedPositions + " Positions. Best value: ");
+        lastBestMove = bestMove;
+
+        Console.WriteLine("Best move is: " + bestMove);
+        
         return bestMove;
     }
 
-    public (Move,int) AlphaBeta(Board board, int depth, int alpha, int beta, bool maximizingPlayer)
+    public (Move,int) AlphaBeta(Board board, int depth, int alpha, int beta, bool maximizingPlayer, Timer timer)
     {
         Move[] legalMoves = board.GetLegalMoves();
         legalMoves = OrderMoves(legalMoves, board);
@@ -42,7 +54,11 @@ public class MyBot : IChessBot
             {
                 board.MakeMove(move);
 
-                var (childMove, eval) = AlphaBeta(board, depth - 1, alpha, beta, false);
+                if (timer.MillisecondsElapsedThisTurn >= timeLimit)
+                {
+                    return (bestMove, maxEval);
+                }
+                var (childMove, eval) = AlphaBeta(board, depth - 1, alpha, beta, false,timer);
                 if (eval > maxEval)
                 {
                     maxEval = eval;
@@ -69,7 +85,11 @@ public class MyBot : IChessBot
             {
                board.MakeMove(move);
 
-               var (childMove, eval) = AlphaBeta(board, depth - 1, alpha, beta, true);
+               if (timer.MillisecondsElapsedThisTurn >= timeLimit)
+               {
+                   return (bestMove, minEval);
+               }
+               var (childMove, eval) = AlphaBeta(board, depth - 1, alpha, beta, true,timer);
                if (eval < minEval)
                {
                    minEval = eval;
@@ -89,9 +109,27 @@ public class MyBot : IChessBot
         }
     }
 
-    public int evaluate(Board board, int remainingDepth)
+    private Move IterativeDeepening(Board board, Timer timer)
     {
-        ulong zobrisKey = board.ZobristKey;
+        Move bestMove = Move.NullMove;
+        for (int depht = 1; depht < MAX_DEPHT; depht++)
+        {
+            if (timer.MillisecondsElapsedThisTurn >= timeLimit)
+            {
+                Console.WriteLine("Best move is: " + bestMove);
+                break;
+            }
+
+            var (move, _) = AlphaBeta(board, depht, int.MinValue, int.MaxValue, isWhite,timer);
+            bestMove = move;
+        }
+
+        return bestMove;
+    }
+
+    private int evaluate(Board board, int remainingDepth)
+    {
+        var zobrisKey = board.ZobristKey;
         
         if (transpositionTable.ContainsKey(zobrisKey) && transpositionTable[zobrisKey].Depht >= remainingDepth)
         {
@@ -100,10 +138,12 @@ public class MyBot : IChessBot
         
 
         evaluatedPositions += 1;
-
-        if (board.IsInCheckmate() || board.IsDraw())
+        //Todo: das funktioniert nicht - spiel endet immer in unentschieden the fuck
+        if (board.IsDraw()) return 0;
+        
+        if (board.IsInCheckmate())
         {
-            if (board.IsWhiteToMove)
+            if (isWhite == board.IsWhiteToMove)
             {
                 return int.MinValue;
             }
@@ -135,7 +175,7 @@ public class MyBot : IChessBot
 
     private Move[] OrderMoves(Move[] moves, Board board)
     {
-        return moves.OrderByDescending(move =>
+        var moveList = moves.OrderByDescending(move =>
         {
             board.MakeMove(move);
             int moveValue = -1000;
@@ -151,7 +191,20 @@ public class MyBot : IChessBot
             board.UndoMove(move);
             return moveValue;
         }).ToArray();
+        //Prepend the last best move to the array
+        return moveList;
+        //todo: funktioniert glaube ich noch nicht richtig
+        if (lastBestMove != Move.NullMove && moveList.Contains(lastBestMove))
+        {
+            Move[] newOrderedMoveList = new Move[moveList.Length+1];
+            newOrderedMoveList[0] = lastBestMove;
+            Array.Copy(moveList,0,newOrderedMoveList,1,moveList.Length);
+            return newOrderedMoveList;
+        }
+
+        
     }
+
 }
 
 public class TranspositionEntry
